e911a19bfa82c105c0504309b1c3a00098e37f87
phiarchitect <phi@phiarchitect.com>
2022-01-16 09:20:34 -0800

    handle dirname

--- sh/melt.py
+++ sh/melt.py
@@ -4,29 +4,40 @@ create melt script to assemble animation
 
 import os as os
 import stat as stat
+import sys as sys
 
-filename = "melt_test.sh"
-fibs = [3, 5, 8, 13, 21, 34, 55, 89, 144, 233]
-fibs = list(reversed(fibs))
+offsets = [3, 5, 8, 13, 21, 34, 55, 89, 144, 233]
+offsets = list(reversed(offsets))
 
-with open(filename, mode="w") as file:
-    os.chmod(filename, stat.S_IRWXU)
+def list_png(dirname):
+    """list files in folder
 
-    cmap_name = "copper"
+    :dirname: TODO
+    :returns: TODO
 
-    file.write("melt ")
+    """
+    filename = dirname + '/melt_build.sh'
 
-    files = os.listdir()
-    files = filter(lambda f: f.endswith('.sh'), files)
-    files = sorted(files)
+    with open(filename, mode='w') as file:
+        os.chmod(filename, stat.S_IRWXU)
 
-    i = 0
+        file.write('melt ')
 
-    for f in files:
-        out = 2
-        if i < len(fibs):
-            out = fibs[i]
+        files = os.listdir(dirname)
+        files = filter(lambda f: f.endswith('.sh'), files)
+        files = sorted(files)
 
-        file.write(f"{f} out={out} ")
-        i+=1
+        i = 0
+
+        for f in files:
+            out = 2
+            if i < len(offsets):
+                out = offsets[i]
+
+            file.write(f'{f} out={out} ')
+            i+=1
+
+if __name__ == '__main__':
+    dirname = sys.argv[1] if sys.argv[1] else '.'
+    list_png(dirname)
 
