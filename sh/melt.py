8cec804a75f037657c84d7f7652fdc2287ae0c66
phiarchitect <phi@phiarchitect.com>
2022-01-17 15:33:43 -0800

    add concat function

--- sh/melt.py
+++ sh/melt.py
@@ -6,10 +6,37 @@ import os as os
 import stat as stat
 import sys as sys
 
-offsets = [3, 5, 8, 13, 21, 34, 55, 89, 144, 233]
+offsets = [3, 5, 8, 13, 21, 34, 55, 89]
 offsets = list(reversed(offsets))
 
-def list_png(dirname):
+def concat_png(dirname):
+    """list files in folder
+
+    :dirname: TODO
+    :returns: TODO
+
+    """
+    filename = dirname + '/concat.txt'
+
+    with open(filename, mode='w') as file:
+
+        files = os.listdir(dirname)
+        files = filter(lambda f: f.endswith('.png'), files)
+        files = sorted(files)
+
+        i = 0
+
+        for f in files:
+            duration = 2
+            if i < len(offsets):
+                out = offsets[i]
+
+            file.write(f'file "{f}" \n')
+            file.write(f'duration "{f}" \n')
+            i+=1
+
+
+def melt_png(dirname):
     """list files in folder
 
     :dirname: TODO
@@ -24,7 +51,7 @@ def list_png(dirname):
         file.write('melt ')
 
         files = os.listdir(dirname)
-        files = filter(lambda f: f.endswith('.sh'), files)
+        files = filter(lambda f: f.endswith('.png'), files)
         files = sorted(files)
 
         i = 0
@@ -38,6 +65,8 @@ def list_png(dirname):
             i+=1
 
 if __name__ == '__main__':
-    dirname = sys.argv[1] if sys.argv[1] else '.'
-    list_png(dirname)
+    dirname = '.'
+    if len(sys.argv) > 1:
+        dirname = sys.argv[1] 
+    melt_png(dirname)
 
